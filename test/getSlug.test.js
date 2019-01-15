9813fa632ae8fe7b28bd9d21cf58a248d4e127fb
Olle Lauri Boström <ollebostr@gmail.com>
2019-01-15 15:31:27 +0100

    Adding tests for getSlug

---
+++ test/getSlug.test.js
@@ -0,0 +1,15 @@
+const getSlug = require('../src/utils/getSlug');
+
+describe('Tests for getSlug', () => {
+  it('generates a correct slug', () => {
+    expect([
+      getSlug('test title 1'),
+      getSlug('Test Title 2'),
+      getSlug('Test-Title 3')
+    ]).toEqual([
+      'test-title-1',
+      'test-title-2',
+      'test-title-3'
+    ])
+  })
+});
