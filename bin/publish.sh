05155c65e6fc3cca5e05d8c8d5b21b325c6f38f1
Olle Lauri Boström <ollebostr@gmail.com>
2019-01-18 18:54:20 +0100

    Simplifying publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Connect to the droplet
-ssh travis@$DROPLET_IP -o StrictHostKeyChecking=no
-echo "[✓] Connected to droplet"
-
 # Stop any running instance
 if [ -d /home/travis/your-scanned-document ]
   then 
@@ -11,18 +7,18 @@ if [ -d /home/travis/your-scanned-document ]
     yarn forever stopall
     cd /home/travis/
     rm -rf /home/travis/your-scanned-document
-    echo "[✓] Stopped running instance"
+    echo "[✓] Stopped running instance \n"
 fi
 
 # Clone the source
 git clone https://github.com/ollelauribostrom/your-scanned-document.git /home/travis/your-scanned-document
 cd /home/travis/your-scanned-document
-echo "[✓] Cloned ollelauribostrom/your-scanned-document"
+echo "[✓] Cloned ollelauribostrom/your-scanned-document \n"
 
 # Install dependencies
 yarn install --production
-echo "[✓] Installed dependencies"
+echo "[✓] Installed dependencies \n"
 
 # Start
 yarn start
-echo "[✓] Started the application"
+echo "[✓] Started the application \n"
