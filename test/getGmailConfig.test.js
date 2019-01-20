7c07d68e9ba23df2b8e7cc2633b9ee875de5e324
Olle Lauri Boström <ollebostr@gmail.com>
2019-01-20 19:36:34 +0100

    Updating failing snapshot test

--- test/getGmailConfig.test.js
+++ test/getGmailConfig.test.js
@@ -1,7 +1,11 @@
+const path = require('path');
 const getGmailConfig = require('../src/utils/getGmailConfig');
+
+jest.mock('path');
 
 describe('Tests for getGmailConfig', () => {
   it('returns a config object', () => {
+    path.resolve = jest.fn(arg => arg);
     expect(getGmailConfig()).toMatchSnapshot();
   });
 });
